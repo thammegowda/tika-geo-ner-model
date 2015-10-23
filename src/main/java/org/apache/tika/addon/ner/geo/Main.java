d62206d098f33736385bef3fd75f502620ac5a0e
Thamme Gowda <tgowdan@gmail.com>
2015-10-23 00:03:21 -0700

    Include content in output

--- src/main/java/org/apache/tika/addon/ner/geo/Main.java
+++ src/main/java/org/apache/tika/addon/ner/geo/Main.java
@@ -1,5 +1,6 @@
 package org.apache.tika.addon.ner.geo;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.tika.Tika;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
@@ -7,6 +8,7 @@ import org.xml.sax.SAXException;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.Arrays;
 
 /**
@@ -27,9 +29,10 @@ public class Main {
         File file = new File(args[0]);
         Tika tika = new Tika();
         Metadata metadata = new Metadata();
-        tika.parse(file, metadata);
-        System.out.println("Metadata : ");
-        //System.out.println(metadata);
+        Reader content = tika.parse(file, metadata);
+        System.out.println(">Content:");
+        System.out.println(IOUtils.readLines(content));
+        System.out.println(">Metadata : ");
         for (String name : metadata.names()) {
             System.out.print(name + "=");
             if (metadata.isMultiValued(name)) {
